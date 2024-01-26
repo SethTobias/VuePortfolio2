c40f9795012dd7c5447e946ee5860db62266eb10
SethTobias <seth.addams1107@gmail.com>
2024-01-26 16:31:35 +0200

    Inital

--- vue-portfolio-sethtobias/src/views/AboutView.vue
+++ vue-portfolio-sethtobias/src/views/AboutView.vue
@@ -100,17 +100,9 @@
 </template>
 
 <script>
-import Navbar from '../components/Navbar.vue'
-import Footer from '../components/Footer.vue'
-import Spinner from '../components/Spinner.vue'
 
 export default {
-  name: 'AboutView',
-  components: {
-    Spinner,
-    Navbar,
-    Footer
-  }
+  name: 'AboutView'
 }
 </script>
 
