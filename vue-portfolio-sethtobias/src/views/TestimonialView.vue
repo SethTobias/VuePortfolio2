956096e85f77ae13ba77ca2b3943765bc26e5502
SethTobias <seth.addams1107@gmail.com>
2024-01-31 13:25:05 +0200

    changes

--- vue-portfolio-sethtobias/src/views/TestimonialView.vue
+++ vue-portfolio-sethtobias/src/views/TestimonialView.vue
@@ -1,18 +1,457 @@
 <template>
-  <div class="home">
-    TestimonialView
-  </div>
-</template>
+  <div class="testimonial-container">
 
+<div class="nav-container">
+<div class="link-items-testimonial one">
+<div class="testimonial-items1">
+<div class="card1 testimonial-card">
+<div class="card1 container-grid">
+<div class="card1 img-container">
+<img src="https://i.ibb.co/L6M5L7R/Ty-noBg.png" alt="" />
+</div>
+<div class="card1 description">
+<div class="card1 description-collapsed">
+<div class="card1 description-collapsed-content">
+<p>Name:</p>
+<p>Tyheisha Johnson</p>
+</div>
+<div class="card1 description-collapsed-content">
+<p>Position:</p>
+<p>Student</p>
+</div>
+<div class="card1 description-collapsed-content">
+<p>Relationship:</p>
+<p>Colleague</p>
+</div>
+</div>
+<div class="card1 description-expanded">
+<p>
+Seth is a charismatic and generous person, he connects with
+everyone in the room. He's analytical and provides help
+whenever he can. He's determined to achieve the best out of
+everyday and motivates those around him as well.
+</p>
+</div>
+</div>
+</div>
+</div>
+<div class="card2 testimonial-card">
+<div class="card2 container-grid">
+<div class="card2 img-container">
+<img
+src="https://lh3.googleusercontent.com/drive-viewer/AEYmBYRcT1LTX-N2xWbobZLA7fZ6H8-BJBIPs9hxnJLf2RbZNgVO7gtXetsaA6cPXaETIduXXg-cKtSaqDIZKmxmcfrd04zi=s2560"
+alt=""
+/>
+</div>
+<div class="card2 description">
+<div class="card2 description-collapsed">
+<div class="card2 description-collapsed-content">
+<p>Name:</p>
+<p>Taryn-lee Walbrugh</p>
+</div>
+<div class="card2 description-collapsed-content">
+<p>Position:</p>
+<p>Student</p>
+</div>
+<div class="card2 description-collapsed-content">
+<p>Relationship:</p>
+<p>Colleague</p>
+</div>
+</div>
+<div class="card2 description-expanded">
+<p>
+Seth Tobias is both kind and compassionate. The atmosphere
+that he brings into one's presence is calming and comforting.
+He has the ability to brighten anyone's day. Seth's work ethic
+and determination is admirable and inspirational.
+</p>
+</div>
+</div>
+</div>
+</div>
+<div class="card3 testimonial-card">
+<div class="card3 container-grid">
+<div class="card3 img-container">
+<img src="https://imgur.com/LyNUFPt" alt="" />
+</div>
+<div class="card3 description">
+<div class="card3 description-collapsed">
+<div class="card3 description-collapsed-content">
+<p>Name:</p>
+<p>Zubaidah Isaacs</p>
+</div>
+<div class="card3 description-collapsed-content">
+<p>Position:</p>
+<p>Student</p>
+</div>
+<div class="card3 description-collapsed-content">
+<p>Relationship:</p>
+<p>Colleague</p>
+</div>
+</div>
+<div class="card3 description-expanded">
+<p>
+Seth is a very friendly person and is very committed to his
+work. He is very hardworking and has good social skills and is
+good at problem-solving and very enthusiastic too.
+</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="link-items-testimonial two">
+<div class="testimonial-items2">
+<div class="card4 testimonial-card">
+<div class="card4 container-grid">
+<div class="card4 img-container">
+<img
+src="https://lh3.googleusercontent.com/drive-viewer/AEYmBYSHyXVmeOjGY7r5JmUj7lh23UykCDPuYkwHQbAqBLqNMlyBaUcgfj36QlBt0HN5WE0WljLycDwG_JDydKbp8kaZxW5q=s2560"
+alt=""
+/>
+</div>
+<div class="card4 description">
+<div class="card4 description-collapsed-content">
+<p>Name:</p>
+<p>Ntokozo Sithebe</p>
+</div>
+<div class="card4 description-collapsed-content">
+<p>Position:</p>
+<p>Student</p>
+</div>
+<div class="card4 description-collapsed-content">
+<p>Relationship:</p>
+<p>Colleague</p>
+</div>
+<div class="card4 description-expanded">
+<p>
+Seth is a very enthusiastic about life. Everything he talks
+about has life or character. He is like a sunflower, always
+blossoming.
+</p>
+</div>
+</div>
+</div>
+</div>
+<div class="card5 testimonial-card">
+<div class="card5 container-grid">
+<div class="card5 img-container">
+<img
+src="https://i.ibb.co/3fwNCwS/My-Home-Pg-Pic-Bg-Square-Ver-no-Bg.png"
+alt=""
+/>
+</div>
+<div class="card5 description">
+<div class="card5 description-collapsed-content">
+<p>Name:</p>
+<p>Naeema Omar</p>
+</div>
+<div class="card5 description-collapsed-content">
+<p>Position:</p>
+<p>Student</p>
+</div>
+<div class="card5 description-collapsed-content">
+<p>Relationship:</p>
+<p>Colleague</p>
+</div>
+<div class="card5 description-expanded">
+<p>
+Seth Tobias always brings great energy to the team. He has
+solid attention to detail which is why he is often designated
+as our go-to debugger. He is self-driven and always go above
+and beyond with any task he's given.
+</p>
+</div>
+</div>
+</div>
+</div>
+<div class="card6 testimonial-card">
+<div class="card6 container-grid">
+<div class="card6 img-container">
+<img
+src="https://lh3.googleusercontent.com/drive-viewer/AEYmBYTZRbjgb8dGHH8yKPYGy1tzTdp3j8JL_XJvL_LZYePVZNVNyGPu52iIMfsC0TPAYIr_9mE0qXgGx6D-npB6x43tdiWMUg=s2560"
+alt=""
+/>
+</div>
+<div class="card6 description">
+<div class="card6 description-collapsed-content">
+<p>Name:</p>
+<p>Matthew Dean Brown</p>
+</div>
+<div class="card6 description-collapsed-content">
+<p>Position:</p>
+<p>Lecturer</p>
+</div>
+<div class="card6 description-collapsed-content">
+<p>Relationship:</p>
+<p>Lecturer</p>
+</div>
+<div class="card6 description-expanded">
+<p></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="nav-link">
+<div class="carousel-item-indicator previous" @click="nextItem(-1)">
+<i class="fa-solid fa-circle-chevron-left fa-2xl" style="color: var(--background)"></i>
+</div>
+<div class="carousel-item-indicator next" @click="nextItem(1)">
+<i class="fa-solid fa-circle-chevron-right fa-2xl" style="color: var(--background)"></i>
+</div>
+</div>
+  </div>
+
+</template>
 <script>
-import Navbar from '../components/Navbar.vue'
-import Footer from '../components/Footer.vue'
+
 
 export default {
-  name: 'TestimonialView',
-  components: {
-    Navbar,
-    Footer
-  }
-}
+  name: "TestimonialView",
+  data() {
+    return {
+      carouselIndex: 1 
+    };
+  },
+  methods: {
+    carouselSetItem() {
+      return this.carouselIndex; 
+    },
+    displayItem(n) {
+      let c = 0;
+      let carouselItems = document.querySelectorAll(".link-items-testimonial");
+      if (n > carouselItems.length) {
+        this.carouselIndex = 1; 
+      }
+      if (n < 1) {
+        this.carouselIndex = carouselItems.length;
+      }
+      for (c = 0; c < carouselItems.length; c++) {
+        carouselItems[c].style.display = "none";
+      }
+      carouselItems[this.carouselIndex - 1].style.display = "flex"; 
+    },
+    currentItem(n) {
+      this.displayItem(this.carouselIndex); 
+    },
+    nextItem(n) {
+      this.displayItem((this.carouselIndex += n)); 
+    },
+  },
+};
 </script>
+
+<style>
+.carousel-main-container {
+  max-width: 1000px;
+position: relative;
+margin: 0 auto;
+}
+.carousel-item {
+  display: none;
+}
+.carousel-item-indicators {
+cursor: pointer;
+position: absolute;
+display: flex;
+justify-content: space-between;
+align-items: center;
+user-select: none;
+}
+.carousel-item-caption {
+  color: #f2f2f2;
+font-size: 15px;
+padding: 8px 12px;
+position: absolute;
+bottom: 8px;
+width: 100%;
+text-align: center;
+}
+.carousel-item-indicator.previous,
+.carousel-item-indicator.next {
+  cursor: pointer;
+z-index: 1;
+align-content: center;
+}
+.carousel-item-index {
+  position: absolute;
+top: 0;
+left: 0;
+}
+
+.nav-container {
+  margin: 0 auto;
+  justify-content: center;
+}
+
+.link-items-testimonial.two {
+display: none;
+}
+.nav-link {
+margin: 0 auto;
+display: flex;
+align-content: center;
+justify-content: space-between;
+max-width: 1250px;
+width: 1250px;
+min-width: 250px;
+}
+</style>
+<style scoped>
+:root {
+--text: #002e28;
+--background: #e0fffa;
+--primary: #008a70;
+--secondary: #5cd1ff;
+--accent: #007cf0;
+}
+
+.testimonial-main-container {
+margin: 0 auto;
+margin-top: 50px;
+max-width: 1250px;
+min-width: 250px;
+}
+
+h2 {
+font-size: 3rem;
+text-align: center;
+}
+
+/* .testimonial-container-grid {
+display: grid;
+grid-row: 1fr / 1fr;
+} */
+
+.testimonial-card {
+background-color: var(--secondary);
+height: fit-content;
+width: fit-content;
+border-radius: 20px;
+border: 5px solid var(--accent);
+margin: 10px auto;
+justify-content: center;
+}
+.link-items-testimonial.one .testimonial-items1,.link-items-testimonial.two .testimonial-items2 {
+/* grid-area: 1/1/2/2; */
+width: 1200px;
+display: flex;
+flex-flow: row wrap;
+justify-content: space-evenly;
+}
+
+.testimonial-items1 .testimonial-card {
+background-color: var(--secondary);
+}
+
+.container-grid {
+height: 400px;
+width: 300px;
+display: grid;
+grid: "img" 75% "description" 25% / 1fr;
+}
+
+.testimonial-items1 .container-grid:hover,.testimonial-items2 .container-grid:hover {
+animation: cardExpand 1s forwards;
+}
+
+@keyframes cardExpand {
+0% {
+grid: "img" 75% "description" 25% / 1fr;
+}
+
+100% {
+grid: "img" 0% "description" 100% / 1fr;
+}
+}
+
+.img-container {
+grid-area: img;
+}
+
+.img-container img {
+width: 100%;
+height: 100%;
+}
+
+.testimonial-items1 .description,.testimonial-items2 .description {
+grid-area: description;
+background-color: var(--primary);
+border-radius: 10px;
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+align-items: center;
+font-size: 20px;
+}
+
+.description-collapsed-content {
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-content: center;
+line-height: 2px;
+}
+
+.description-expanded {
+text-align: center;
+padding: 0 25px;
+font-size: 20px;
+}
+.testimonial-items1 .description-expanded, .testimonial-items2 .description-expanded{
+overflow: hidden;
+}
+
+.testimonial-items2 {
+grid-area: 1/1/2/2;
+display: flex;
+flex-flow: row wrap;
+justify-content: space-evenly;
+}
+
+.testimonial-items2 .testimonial-card {
+background-color: var(--primary);
+border-radius: 10px;
+}
+
+.testimonial-items2 .description {
+grid-area: description;
+background-color: var(--secondary);
+border-radius: 10px;
+text-align: center;
+padding: 0 25px;
+}
+
+.testimonial-items2 .description-expanded {
+display: block;
+}
+
+/* width
+::-webkit-scrollbar {
+width: 5px;
+} */
+
+/* Track
+::-webkit-scrollbar-track {
+box-shadow: inset 0 0 5px var(--text);
+border-radius: 10px;
+} */
+
+/* Handle
+::-webkit-scrollbar-thumb {
+background: var(--text);
+border-radius: 10px;
+} */
+
+@media screen and (width<300px) {
+}
+
+.testimonial-container {
+width: 250px;
+margin: 0 auto
+}
+</style>
